bd82b13cfdd7b3f7a9cd58a4d81a0e04da20bc25
AlecitoUAB <alex.navarroD@e-campus.uab.cat>
2022-01-06 19:10:16 +0100

    Punto 3 terminado

---
+++ lib/page_form_project.dart
@@ -0,0 +1,90 @@
+import 'package:codelab_timetracker/requests.dart';
+import 'package:codelab_timetracker/tree.dart' hide getTree;
+import 'package:flutter/material.dart';
+import 'package:codelab_timetracker/page_activities.dart';
+
+
+
+class FormPageProject extends StatefulWidget {
+  @override
+  _FormPageProjectState createState() => _FormPageProjectState();
+  final int id;
+  FormPageProject(this.id);
+}
+
+class _FormPageProjectState extends State<FormPageProject> {
+  late int id;
+  late Future<Tree> futureTree;
+  final _formKey = GlobalKey<FormState>();
+  final name = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    id = widget.id;
+    futureTree = getTree(id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crear Proyecto'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.home),
+              onPressed: () {
+                while(Navigator.of(context).canPop()) {
+                  print("pop");
+                  Navigator.of(context).pop();
+                }
+                /* this works also:
+                      Navigator.popUntil(context, ModalRoute.withName('/'));
+                      */
+                PageActivities(0);
+              }),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              _crearInputNom(),
+
+              FlatButton(
+                onPressed: () {
+                  if(_formKey.currentState!.validate()) {
+                    print("Proyecto creado correctamente");
+                    addProject(id, name.text);
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => PageActivities(id)),
+                    );
+                  } else {
+                    print("Error");
+                  }
+                },
+                color: Colors.blue,
+                child: Text('Crear', style:TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _crearInputNom() {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: TextFormField(
+        controller: name,
+        validator: (value) => value!.isEmpty ? "campo requerido" : null,
+        decoration: InputDecoration(
+        labelText: 'Introducir nombre del proyecto',
+        hintText: 'Nombre',
+        ),
+      ),
+    );
+  }
+}
+
+
---
+++ lib/page_form_task.dart
@@ -0,0 +1,88 @@
+import 'package:codelab_timetracker/requests.dart';
+import 'package:codelab_timetracker/tree.dart' hide getTree;
+import 'package:flutter/material.dart';
+import 'package:codelab_timetracker/page_activities.dart';
+
+
+
+class FormPageTask extends StatefulWidget {
+  @override
+  _FormPageTaskState createState() => _FormPageTaskState();
+  final int id;
+  FormPageTask(this.id);
+}
+
+class _FormPageTaskState extends State<FormPageTask> {
+  late int id;
+  late Future<Tree> futureTree;
+  final _formKey = GlobalKey<FormState>();
+  final name = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    id = widget.id;
+    futureTree = getTree(id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crear Tarea'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.home),
+              onPressed: () {
+                while(Navigator.of(context).canPop()) {
+                  print("pop");
+                  Navigator.of(context).pop();
+                }
+                /* this works also:
+                      Navigator.popUntil(context, ModalRoute.withName('/'));
+                      */
+                PageActivities(0);
+              }),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              _crearInputNom(),
+
+              FlatButton(
+                onPressed: () {
+                  if(_formKey.currentState!.validate()) {
+                    print("Tarea creado correctamente");
+                    addTask(id, name.text);
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => PageActivities(id)),
+                    );
+                  } else {
+                    print("Error");
+                  }
+                },
+                color: Colors.blue,
+                child: Text('Crear', style:TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _crearInputNom() {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: TextFormField(
+        controller: name,
+        validator: (value) => value!.isEmpty ? "campo requerido" : null,
+        decoration: InputDecoration(
+          labelText: 'Introducir nombre de la Tarea',
+          hintText: 'Nombre',
+        ),
+      ),
+    );
+  }
+}
